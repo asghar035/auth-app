26ffb8b5450b19bce5467e86f51b62224b6153b2
Ihtisham ul haq <khihtisham304@gmail.com>
2025-09-01 12:46:18 +0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require ('./routes/AuthRouter')
+const ProductRouter = require ('./routes/ProductRouter')
+
+require("./models/db");
+   
+const PORT = process.env.PORT || 8080;
+
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api/auth",AuthRouter);
+app.use("/products",ProductRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`app is running on ${PORT} `);
+});
